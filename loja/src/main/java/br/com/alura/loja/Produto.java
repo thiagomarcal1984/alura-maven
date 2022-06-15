cf8e446f3f3b983fd3c09d14e59198bd7e6c091a
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-06-15 13:49:01 -0300

    Plugins.

--- loja/src/main/java/br/com/alura/loja/Produto.java
+++ loja/src/main/java/br/com/alura/loja/Produto.java
@@ -1,5 +1,20 @@
 package br.com.alura.loja;
 
-public class Produto {
+import java.math.BigDecimal;
 
+public class Produto {
+	private String nome;
+	private BigDecimal preco;
+	
+	public Produto(String nome, BigDecimal preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
 }
